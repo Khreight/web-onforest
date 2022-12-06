d6884713e4cf6616acfd89143dfd3c7721f48cf9
khreight <louisthedeaf@gmail.com>
2022-12-06 23:07:02 +0100

    Début du projet

---
+++ Pages/acceuil.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OnForest | Serveur Minecraft</title>
+    <link rel="icon" type="image/x-icon" href="../Img/logo.ico">
+    <link rel="stylesheet" href="../Css/acceuil.css">
+    <link rel="stylesheet" href="../Css/navbar.css">
+</head>
+<body>
+    
+<nav class="menu-container">
+  <input type="checkbox" aria-label="Toggle menu" />
+  <span></span>
+  <span></span>
+  <span></span>
+  <div class="menu">
+    <ul>
+      <li>
+        <a href="./acceuil.php">
+          Acceuil
+        </a>
+      </li>
+      <li>
+        <a href="./information.php">
+          Information
+        </a>
+      </li>
+      <li>
+        <a href="./boutique.php">
+          Boutique
+        </a>
+      </li>
+    </ul>
+    <ul>
+    </ul>
+  </div>
+</nav>
+
+    <div class="mainone">
+        <div class="txt">
+            <div class="just">
+                <h2>Prêt à jouer sur</h2>
+                <h1>OnForest?</h1>
+            </div>
+        </div>
+        <div class="rect">
+            <div class="img">
+                <img src="../Img/logo.png" alt="">
+            </div>
+            <div class="ip">
+                <h2>IP: game-0.lifeheberg.com</h2>
+                <h2>Port: 25584</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="maintwo">
+        <div class="videoone">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/L2DkmGCPvF8?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h2>Découvrez également notre dernière vidéo...</h2>
+        </div>
+    </div>
+
+    <div class="mainthree">
+        <h1>Mais, qu'est ce que OnForest?</h1>
+        <h3>OnForest est un serveur Minecraft qui se base sur un grand concept: le PVP Faction ainsi qu'une arène de FFA. OnForest possède une communauté qui surkiffe le PvpFaction. Nous possèdons également un Staff complet, actif et à votre écoute. Vous avez une question, une plainte, une suggestion,... Adressez-vous directement à eux, ils seront bien content de vous y répondre.</h3>
+    </div>
+
+    <div class="mainfour">
+        <h1>Donc n'attendez plus! Venez nous rejoindre via nos diffèrents canaux de communications ainsi que en jeu!</h1>
+        <h2>↪ <a href="https://discord.gg/GspuCWnxee" target="_blank">Discord</a></h2>
+        <h2>↪ <a href="https://www.youtube.com/@Onforest/?sub_confirmation=1" target="_blank">Youtube</a></h2>
+        <h2>↪ <a href="https://www.instagram.com/onforest.faction.mc/" target="_blank">Instagram</a></h2>
+        <h2>↪ <a href="mailto:OnForest@outlook.fr">Mail</a></h2>
+        
+    </div>
+
+
+</body>
+</html>
